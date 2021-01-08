b250d3e0b8e3a5e29943a6f6bb19ed8e8f8f9077
s3zportfolio <s3zportfolio@outlook.com>
2021-01-08 15:32:31 +0000

    Index page update

--- RTZResourceManager/RTZResourceManagerApp/urls.py
+++ RTZResourceManager/RTZResourceManagerApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='index'),
+    path('HomePage/',views.HomePage,name='homepage'),
 ]
--- RTZResourceManager/RTZResourceManagerApp/views.py
+++ RTZResourceManager/RTZResourceManagerApp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+def HomePage(request):
+    return render(request,'homepage.html')

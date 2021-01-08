9393e401b477806cad00d7d87c3c7429b4c3da24
s3zportfolio <s3zportfolio@outlook.com>
2021-01-08 12:50:29 +0000

    Index page

--- RTZResourceManager/RTZResourceManager/settings.py
+++ RTZResourceManager/RTZResourceManager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RTZResourceManagerApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'RTZResourceManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['RTZResourceManagerApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

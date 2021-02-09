240d368aa89c87753b123227d86887e4b89a9bb5
s3zportfolio <s3zportfolio@outlook.com>
2021-02-09 00:22:50 +0000

    database creation-Employeedetails.

--- RTZResourceManager/EmployeeManagementApp/models.py
+++ RTZResourceManager/EmployeeManagementApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class EmployeeDetails(models.Model):
+    empId=models.IntegerField(primary_key=True)
+    first_name=models.CharField(max_length=45)
+    last_name=models.CharField(max_length=45)
+    dateofbirth=models.DateTimeField()
+    email=models.CharField(max_length=200)
+    phone=models.CharField(max_length=30)
+    address=models.CharField(max_length=500)
+    employeetype=models.CharField(max_length=1)
+    salarytype=models.CharField(max_length=1)
+    startdate=models.DateTimeField()
+    enddate=models.DateTimeField()
--- RTZResourceManager/RTZResourceManager/settings.py
+++ RTZResourceManager/RTZResourceManager/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'RTZResourceManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['RTZResourceManagerApp/templates'],
+        'DIRS': ['RTZResourceManager/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'RTZResourceManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rtzresmgr',
+        'USER' :'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

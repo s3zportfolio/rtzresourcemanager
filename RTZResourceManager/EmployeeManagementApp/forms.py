d3df4ca5edfcad8ef4eeb1316aa29ac0b67ffc10
s3zportfolio <s3zportfolio@outlook.com>
2021-02-12 00:08:40 +0000

    Update empl type forms

---
+++ RTZResourceManager/EmployeeManagementApp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import EmployeeType
+
+
+class EmpTypeForm(forms.ModelForm):
+    class Meta:
+        model=EmployeeType
+        fields =('EmpTypeCode','EmployeeType')

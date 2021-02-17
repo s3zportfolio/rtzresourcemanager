049f6377f0c6340409383265010c4b510d6832a6
s3zportfolio <s3zportfolio@outlook.com>
2021-02-17 21:37:11 +0000

    emp type update

--- RTZResourceManager/AdministrationApp/views.py
+++ RTZResourceManager/AdministrationApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponseRedirect
 from .forms import EmpTypeForm
 from .models import EmployeeType
 
@@ -12,12 +12,14 @@ def employeetype(request):
         form=EmpTypeForm(request.POST)
         if form.is_valid():
             form.save()
+            form=EmpTypeForm()
+
             #return redirect('empType.html')
     else:
-            form=EmpTypeForm()
+        form=EmpTypeForm()
     #return render(request,'empType.html',{'form':form})
     allEmpTypes=EmployeeType.objects.all()
     return render(request,'empType.html',{
-      'allEmpTypes':allEmpTypes,
-      'form':form,
+        'form':form,
+      'allEmpTypes':allEmpTypes
       })

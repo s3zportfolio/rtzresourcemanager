ebed649da7331c595821be34f065f19cbfa64872
s3zportfolio <s3zportfolio@outlook.com>
2021-02-17 00:53:45 +0000

    Emptype

--- RTZResourceManager/AdministrationApp/views.py
+++ RTZResourceManager/AdministrationApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import EmpTypeForm
+from .models import EmployeeType
 
 # Create your views here.
 def home(request):
@@ -14,6 +15,13 @@ def employeetype(request):
             return redirect('empType.html')
     else:
             form=EmpTypeForm()
-    return render(request,'empType.html',{'form':form})
-
+    #return render(request,'empType.html',{'form':form})
+    allEmpTypes=EmployeeType.objects.all()
+    return render(request,'empType.html',{
+      'allEmpTypes':allEmpTypes,
+      'form':form,
+      })
+#def ListEmployeeType(request):
+#    allEmpTypes=EmployeeType.objects.all()
+#    return render(request,'empType.html',{'allEmpTypes':allEmpTypes})
 # Create your views here.
